fa949b15965ea52dae157f77db9f6d49df9f3dc3
galliotl <lucas.galliot@utt.fr>
2020-01-15 11:42:10 +0100

    ui QRCode workflow is done

--- lib/ui/pages/account/AccountPage.dart
+++ lib/ui/pages/account/AccountPage.dart
@@ -2,6 +2,9 @@ import 'package:barty/ui/components/navbars/AccountNavbar.dart';
 import 'package:flutter/material.dart';
 
 class AccountPage extends StatelessWidget {
+  
+  static final routeName = '/account';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
