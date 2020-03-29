f5e13eb1dd4a4e22cbcbf10095a68e9c5db9252b
Lorenzo <l.mevellec@gmail.com>
2020-03-29 15:39:25 +0200

    deleted useless regex class

--- backend/src/utils/regex.ts
+++ backend/src/utils/regex.ts
@@ -1,8 +1,3 @@
-export class Regex{
-  letterAndNumber= /^([a-z0-9]{1,})$/;
-  phoneNumber= /^([+-]{0,1}[0-9]{10,13})$/;
-}
-
 export const verifyRegexPhone = (
     phone: string
   ): Boolean => {
