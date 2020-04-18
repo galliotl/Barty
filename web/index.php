a7a19af487931f89a79c03046bd79d0e8195517e
Lorenzo <l.mevellec@gmail.com>
2020-04-18 21:31:43 +0200

    première version de  l'index: terminé !

--- web/index.php
+++ web/index.php
@@ -46,7 +46,7 @@
         </div>
         <div style="height: 0; margin-top: 350px" class="row">
             <div style="padding-top: 10px; padding-bottom: 10px;" class="col-12">
-                <button style="height: 80px; width: 10em; margin-left: 100px" type="button" class="btn btn-primary">GET
+                <button style="height: 80px; width: 10em; margin-left: 100px; border-radius: 30px" type="button" class="btn btn-primary">GET
                     THE
                     APP
                 </button>
@@ -57,9 +57,11 @@
 
 
 <div style=" background-color: black; padding-top: 100px" class="container-fluid">
+    <!-- premier bandeau -->
     <div class="row">
         <div class="col-lg-4 offset-lg-1">
-            <p style="color: white; margin-top: 100px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum pretium
+            <p style="color: white; margin-top: 100px">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur bibendum pretium
                 tortor. In facilisis erat tortor, vel posuere libero finibus vel. Vestibulum accumsan et mi sit amet
                 gravida. Aliquam nibh mauris, scelerisque at congue sit amet, consectetur pulvinar magna. Curabitur sed
                 lectus quis augue volutpat molestie. Aliquam mi lacus, sagittis a diam ac, dapibus dictum risus.
@@ -72,6 +74,68 @@
         </div>
         <div class="col-lg-4 offset-lg-2 text-center">
             <img style="max-width: 100%" src="images/pages/index/beer.jpg">
+        </div>
+    </div>
+    <!-- bouton -->
+    <div style="margin-top: 100px;" class="row">
+        <a href="#" style="margin-left: 50%; text-decoration: none" class="multicolor_button">get the app</a>
+    </div>
+    <!-- second bandeau -->
+    <div style="margin-top: 10px" class="row">
+        <div class="col-lg-4 offset-lg-1 text-center">
+            <img style="max-width: 100%" src="images/pages/index/beer.jpg">
+        </div>
+        <div class="col-lg-4 offset-lg-2">
+            <p style="color: white; margin-top: 100px">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur bibendum pretium
+                tortor. In facilisis erat tortor, vel posuere libero finibus vel. Vestibulum accumsan et mi sit amet
+                gravida. Aliquam nibh mauris, scelerisque at congue sit amet, consectetur pulvinar magna. Curabitur sed
+                lectus quis augue volutpat molestie. Aliquam mi lacus, sagittis a diam ac, dapibus dictum risus.
+                Suspendisse eu tristique ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia Curae; Morbi non nulla hendrerit, convallis leo sit amet, consectetur lectus. Praesent
+                fermentum, metus in iaculis porttitor, ligula magna ullamcorper tortor, id iaculis elit libero et nisi.
+                Vestibulum semper urna at bibendum tristique. Ut non nulla eget neque pretium commodo in sit amet risus.
+                Maecenas non quam libero. Sed efficitur tortor non velit iaculis, vel egestas est malesuada. Phasellus
+                et enim vehicula, vulputate dolor in, ultricies est.</p>
+        </div>
+    </div>
+</div>
+<div style="background-color: black; padding-top: 50px; padding-bottom: 20px" class="container-fluid">
+    <hr style="background-color: white">
+
+    <div class="row">
+        <div class="col-6 text-center">
+            <a style="padding: 5px" href="#">
+                <h5 style="color: white">Nous contacter</h5>
+            </a>
+            <a style="padding: 5px" href="#">
+                <h5 style="color: white">Conditions Générales d'Utilisation</h5>
+            </a>
+            <a style="padding: 5px" href="#">
+                <h5 style="color: white">Conditions Générales de Vente</h5>
+            </a>
+            <a style="padding: 5px" href="#">
+                <h5 style="color: white">Crédits</h5>
+            </a>
+        </div>
+        <div class="col-6 text-center">
+            <div class="col-12">
+                <button style="height: 50px; width: 10em; margin-top: 50px" type="button" class="btn btn-primary">
+                    Google
+                </button>
+            </div>
+            <div class="col-12">
+                <button style="height: 50px; width: 10em; margin-top: 50px" type="button" class="btn btn-primary">
+                    Apple
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 text-center">
+            <a style="padding: 5px" href="#">
+                <h5 style="color: white">Copyright</h5>
+            </a>
         </div>
     </div>
 </div>
