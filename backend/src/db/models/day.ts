d491d227e64b7374a516ec1531f27c9dbcb48d0f
Timothé Nicole <tim@MacBook-Pro-de-Timothe.local>
2020-06-02 18:07:57 +0200

    le bar peut être fermé un jour de la semaine

--- backend/src/db/models/day.ts
+++ backend/src/db/models/day.ts
@@ -4,9 +4,11 @@ import { Time, timeSchema } from './time';
 export interface Day extends mongoose.Document {
   openningHour: Time;
   closingHour: Time;
+  ferme: boolean;
 }
 
 export const daySchema = new mongoose.Schema({
   openingHour: { type: timeSchema },
   closingHour: { type: timeSchema },
+  ferme: {type: Boolean, default: false},
 });
